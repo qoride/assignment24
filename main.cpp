2fb5cf9da855e1c8523975f1cd38723a0b1d8157
qoride <qoride@gmail.com>
2024-10-21 18:45:41 +0000

    inputs done

--- main.cpp
+++ main.cpp
@@ -3,50 +3,98 @@
 
 using namespace std;
 
+const char delimeter[2] = {'@','.'};
+const string domains[6] = {"com","org","gov","net","edu","mil"};
+
+struct Customer{
+    int ID;
+    string name;
+    string email;
+};
+
 int main(){
-    int val, inputs = 0, sum = 0;
+    bool accepted;
+    int val;
+    string input;
+    Customer user;
 
-    cout << "Enter a list of integers seperated by spaces: ";
-    
-    int* num = new int[20];
+    cout << "Enter your ID: ";
+    user.ID = -1;
 
-    while(true){
+    while(user.ID == -1){
         while(cin >> val){
             if(cin.good()){
-                if(inputs%20==0&&inputs!=0){
-                    int* resize = new int[inputs+20];
-                    for(int i = 0; i < inputs; i++){
-                        resize[i] = num[i];
-                    }
-                    num = resize;
-                    delete[] resize;
-                }
-                num[inputs] = val;
-                inputs++;
-            }
-            if(cin.peek() == '\n'){
-                cout << "\nEnter more values or type a string to end input: ";
+                user.ID = val;
+                break;
             }
         }
         if(cin.fail()){
             cin.clear();
-            cout << "\nERROR: A value was not a number, ending...";
-            break;
+            cout << "\nERROR: ID was not accepted, try again: ";
             cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
             cin.clear();
         }
     }
 
-    cout << "\nInputs in array: ";
+    cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    cin.clear();
 
-    for(int i = 0; i < inputs; i++){
-        cout << num[i] << ((i == inputs-1)?"":", ");
-        sum += num[i];
+    cout << "\nEnter your name: ";
+
+    getline(cin,input);
+    user.name = input;
+
+    cout << "\nEnter your e-mail: ";
+    user.email = "null";
+
+    while(user.email == "null"){
+        while(cin >> input){
+            if(cin.good()){
+                accepted = false;
+                if(input.size()>=7){
+                    for(int i = 0; i < input.size(); i++){
+                        if(input[i] == delimeter[1]){
+                            break;
+                        }
+                        if(input[i] == delimeter[0]){
+                            for(int j = i+1; j < input.size(); j++){
+                                if(input[j] == delimeter[0]){
+                                    break;
+                                }
+                                if(input[j] == delimeter[1]){
+                                    for(int d = 0; d < 6; d++){
+                                        if(input.substr(j+1,input.size())==domains[d]){
+                                            accepted = true;
+                                            user.email = input;
+                                            break;
+                                        }
+                                    }
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                if(accepted == false){
+                    cout << "\nERROR: E-mail was not accepted, try again: ";
+                }else{
+                    break;
+                }
+            }
+        }
+        if(cin.fail()){
+            cin.clear();
+            cout << "\nERROR: E-mail was not accepted, try again: ";
+            cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            cin.clear();
+        }
+        if(accepted == true)break;
     }
 
-    delete[] num;
-
-    cout << "\n\nThe sum of all the inputs is " << sum << ".";
+    cout << "\nID: " << user.ID
+    << "\nName: " << user.name
+    << "\nE-mail: " << user.email << endl;
 
     return 0;
 }
